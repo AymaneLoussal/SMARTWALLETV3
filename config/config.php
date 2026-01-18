2d74c1fe4564d5b614d09c58794da6d947da36f4
Aymane Loussal <aymanloussal552@gmail.com>
2026-01-18 21:18:32 +0100

    feat(config): define db/app configuration

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+// Database Configuration
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'smartWallet');
+define('DB_USER', 'postgres');
+define('DB_PASS', 'aaaymaaan');
+
+// Application Configuration
+define('BASE_URL', 'http://localhost/smartWalletMvc/public');
+define('APP_NAME', 'Smart Wallet');
+
+// Debug Mode (set to false in production)
+define('APP_DEBUG', true);
